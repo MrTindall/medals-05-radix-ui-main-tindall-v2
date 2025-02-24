e30e5271661f7939b1641f6beed62d13cbeaa917
Alex <tindall.al@gmail.com>
2025-02-24 17:48:49 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/medals-05-radix-ui-main-tindall/',
+  base: '/medals-05-radix-ui-main-tindall-v2/',
 })
 
 
